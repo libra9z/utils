29aa7d177dadc259b4920cf17eb35f5d04f140b7
eric <libra9z@163.com>
2017-02-25 13:08:43 +0800

    增加service之间互相调用接口（REST）

---
+++ service.go
@@ -0,0 +1,98 @@
+package utils
+
+import (
+	"strings"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"net/http"
+	"errors"
+)
+
+func ServicePost(uri string,params string)(interface{},error) {
+
+	resp, err := http.Post(uri,"application/x-www-form-urlencoded",strings.NewReader(params))
+
+	if resp == nil || err != nil  {
+		fmt.Printf("no response body return or has error(%v).\n",err)
+		return 0,errors.New("no response body return or has error")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		fmt.Println(string(body))
+		return 0,err
+	}
+
+
+	var vs map[string]interface{}
+	err = json.Unmarshal(body,&vs)
+
+	if err != nil {
+		fmt.Printf("cannot convert json.\n")
+		return 0,err
+	}
+
+	return vs,err
+}
+
+func ServicePut(uri string,params string)(interface{},error) {
+
+	client := &http.Client{}
+	req,err := http.NewRequest(http.MethodPut,uri,strings.NewReader(params))
+	resp, err := client.Do(req)
+
+	if resp == nil || err != nil  {
+		fmt.Printf("no response body return or has error(%v).\n",err)
+		return 0,errors.New("no response body return or has error")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		fmt.Println(string(body))
+		return 0,err
+	}
+
+
+	var vs map[string]interface{}
+	err = json.Unmarshal(body,&vs)
+
+	if err != nil {
+		fmt.Printf("cannot convert json.\n")
+		return 0,err
+	}
+
+	return vs,err
+}
+
+func ServiceDelete(uri string,params string)(interface{},error) {
+
+	client := &http.Client{}
+	req,err := http.NewRequest(http.MethodDelete,uri,strings.NewReader(params))
+	resp, err := client.Do(req)
+
+	if resp == nil || err != nil  {
+		fmt.Printf("no response body return or has error(%v).\n",err)
+		return 0,errors.New("no response body return or has error")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		fmt.Println(string(body))
+		return 0,err
+	}
+
+
+	var vs map[string]interface{}
+	err = json.Unmarshal(body,&vs)
+
+	if err != nil {
+		fmt.Printf("cannot convert json.\n")
+		return 0,err
+	}
+
+	return vs,err
+}
