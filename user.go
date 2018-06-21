18be9912916d292a32fa0b245231f70cc7dfaebe
eric <libra9z@163.com>
2018-06-21 09:47:55 +0800

    token 检查增加 userid的检查

--- user.go
+++ user.go
@@ -187,6 +187,52 @@ func CheckToken(uri string,appid int,token string) bool {
 	return bret	
 }
 
+func CheckToken2(uri string,appid int,token string) (int64,bool) {
+
+	var bret bool = false
+
+	resp, err := http.Post(uri,"application/x-www-form-urlencoded",strings.NewReader(""))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if resp == nil {
+		fmt.Printf("no response body return.\n")
+		return 0,false
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		fmt.Println(string(body))
+	}
+
+
+	var vs map[string]interface{}
+	err = json.Unmarshal(body,&vs)
+
+	if err != nil {
+		fmt.Printf("cannot convert json.\n")
+		return 0,false
+	}
+
+	//fmt.Printf("body = %v.\n",vs)
+	var userid int64
+	if vs["Ret"] != nil {
+		ret := Convert2Int64(vs["Ret"])
+		if vs["Userid"] != nil {
+			userid = Convert2Int64( vs["Userid"])
+			if ret == 0 && userid>0 {    //userid>0 表示用户已登录
+				bret = true
+			}
+		}
+	}
+
+	return userid,bret
+}
+
+
 func GetBirthdayFromIdcard( idcard string) string {
 	if idcard == "" {
 		return ""
