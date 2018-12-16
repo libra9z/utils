0bd1d2d025627c813a3269c44fbad0f09dd0dd2c
eric <libra9z@163.com>
2018-12-16 13:11:04 +0800

    增加方法

--- convert.go
+++ convert.go
@@ -1,10 +1,10 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"reflect"
 	"strconv"
-	"fmt"
-	"encoding/json"
 )
 
 func Convert2Int64(v interface{}) int64 {
@@ -16,7 +16,7 @@ func Convert2Int64(v interface{}) int64 {
 	var ret int64
 	switch t.Name() {
 	case "string":
-		ret,_ = strconv.ParseInt(v.(string),10,64)
+		ret, _ = strconv.ParseInt(v.(string), 10, 64)
 	case "float64":
 		ret = int64(v.(float64))
 	case "int64":
@@ -28,7 +28,6 @@ func Convert2Int64(v interface{}) int64 {
 	return ret
 }
 
-
 func Convert2Int(v interface{}) int {
 
 	if v == nil {
@@ -39,7 +38,7 @@ func Convert2Int(v interface{}) int {
 	var ret int64
 	switch t.Name() {
 	case "string":
-		ret,_ = strconv.ParseInt(v.(string),10,64)
+		ret, _ = strconv.ParseInt(v.(string), 10, 64)
 	case "float64":
 		ret = int64(v.(float64))
 	case "int64":
@@ -51,6 +50,50 @@ func Convert2Int(v interface{}) int {
 	return int(ret)
 }
 
+func Convert2Int16(v interface{}) int16 {
+
+	if v == nil {
+		return 0
+	}
+
+	t := reflect.TypeOf(v)
+	var ret int64
+	switch t.Name() {
+	case "string":
+		ret, _ = strconv.ParseInt(v.(string), 10, 64)
+	case "float64":
+		ret = int64(v.(float64))
+	case "int64":
+		ret = v.(int64)
+	case "int":
+		ret = int64(v.(int))
+	}
+
+	return int16(ret)
+}
+
+func Convert2Int8(v interface{}) int8 {
+
+	if v == nil {
+		return 0
+	}
+
+	t := reflect.TypeOf(v)
+	var ret int64
+	switch t.Name() {
+	case "string":
+		ret, _ = strconv.ParseInt(v.(string), 10, 64)
+	case "float64":
+		ret = int64(v.(float64))
+	case "int64":
+		ret = v.(int64)
+	case "int":
+		ret = int64(v.(int))
+	}
+
+	return int8(ret)
+}
+
 func Convert2Float64(v interface{}) float64 {
 	if v == nil {
 		return 0
@@ -60,7 +103,7 @@ func Convert2Float64(v interface{}) float64 {
 	var ret float64
 	switch t.Name() {
 	case "string":
-		ret,_ = strconv.ParseFloat(v.(string),64)
+		ret, _ = strconv.ParseFloat(v.(string), 64)
 	case "float64":
 		ret = v.(float64)
 	case "int64":
@@ -82,7 +125,7 @@ func Convert2Float32(v interface{}) float32 {
 	var ret1 float64
 	switch t.Name() {
 	case "string":
-		ret1,_ = strconv.ParseFloat(v.(string),64)
+		ret1, _ = strconv.ParseFloat(v.(string), 64)
 		ret = float32(ret1)
 	case "float64":
 		ret1 = v.(float64)
@@ -107,32 +150,32 @@ func ConvertToString(v interface{}) string {
 	case "string":
 		ret = v.(string)
 	case "int64":
-		ret = strconv.FormatInt(v.(int64),10)
+		ret = strconv.FormatInt(v.(int64), 10)
 	case "float64":
-		ret = strconv.FormatFloat(v.(float64),'f',-1,64)
+		ret = strconv.FormatFloat(v.(float64), 'f', -1, 64)
 	default:
-		bb,err:=json.Marshal(v)
+		bb, err := json.Marshal(v)
 		if err != nil {
-			fmt.Errorf("json 格式不正确 ：%v",err)
+			fmt.Errorf("json 格式不正确 ：%v", err)
 			return ""
 		}
-		ret=string(bb)
+		ret = string(bb)
 	}
 
 	return ret
 }
 
-func StructToMap(v interface{})(map[string]interface{},error){
-	js,err := json.Marshal( v )
+func StructToMap(v interface{}) (map[string]interface{}, error) {
+	js, err := json.Marshal(v)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	m := map[string]interface{}{}
 
-	err = json.Unmarshal(js,&m)
+	err = json.Unmarshal(js, &m)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	return m,nil
+	return m, nil
 }
